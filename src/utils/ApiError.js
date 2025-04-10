d3d7ddeecc37db3fa67c28ec328417991517df7e
Harish Kumar <harishkumarr193@gmail.com>
2025-04-10 22:05:57 +0530

    add utils

---
+++ src/utils/ApiError.js
@@ -0,0 +1,23 @@
+class ApiError extends Error {
+  constructor(
+    statusCode,
+    message = "something went wrong",
+    errors = [],
+    stack = "" //Stack trace of where the error occurred (helps in debugging).
+  ) {
+    super(message);
+    this.statusCode = statusCode;
+    this.data = null;
+    this.message = message;
+    this.success = false;
+    this.errors = errors;
+
+    if (stack) {
+      this.stack = stack;
+    } else {
+      Error.captureStackTrace(this, this.constructor);
+    }
+  }
+}
+
+return { ApiError };
